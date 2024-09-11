a4f3e6e9685f82df795104cf794c23b3a0fb1876
jf3079 <jerome3079@gmail.com>
2024-09-11 12:03:10 -0400

    udpated

--- 0. Provider.tf
+++ 0. Provider.tf
@@ -14,6 +14,17 @@ terraform {
 }
 
 
+terraform {
+  required_providers {
+    awsutils = {
+      source = "cloudposse/awsutils"
+      # For local development,
+      # install the provider on local computer by running `make install` from the root of the repo, and uncomment the 
+      # version below
+      # version = "9999.99.99"
+    }
+  }
+}
 # Delete the default VPC in our account/region
 resource "awsutils_default_vpc_deletion" "default" {
 }
